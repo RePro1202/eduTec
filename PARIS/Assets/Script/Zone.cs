b41c6e5b4e79fdf071cf7c020060472a12bb93a4
RePro1202 <78546867+RePro1202@users.noreply.github.com>
2021-11-04 18:02:09 +0900

    Test
    충돌 무시,
    이벤트 발생 지점 표시

---
+++ PARIS/Assets/Script/Zone.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zone : MonoBehaviour
+{
+
+    public GameObject ZonePopup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ZonePopup.SetActive(false);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            ZonePopup.SetActive(true);
+        }
+    }
+}
